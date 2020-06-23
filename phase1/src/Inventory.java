cc83e0769d28cc7425f90d88a81dbfbc9a132ecd
yiyun Ding <amydingyiyun@gmail.com>
2020-06-23 00:53:06 -0400

    Inventory

--- phase1/src/Inventory.java
+++ phase1/src/Inventory.java
@@ -4,19 +4,34 @@ public class Inventory {
     ArrayList<Item> lendingList;
 
 
+    /**
+     * Constructor, inventory has an instance variable lendingList.
+     */
     public Inventory() {
         lendingList = new ArrayList<Item>();
     }
 
-
+    /**
+     * getter for the lending list
+     * @return lendingList
+     */
     public ArrayList<Item> getLendingList() {
         return lendingList;
     }
 
+    /**
+     * add the item into the inventory (when clientuser adds Item to the lending list, inventory should add the item)
+     * @param item the item added
+     */
     public void addItem(Item item){
         lendingList.add(item);
     }
 
+    /**
+     * delete the item from the inventory (when a trade is completed or clentUser remove the item from the lendinglist,
+     * the item should be removed from the inventory.
+     * @param item the item that should be deleted
+     */
     public void deleteItem(Item item){
         if (lendingList.contains(item)){
             lendingList.remove(item);
