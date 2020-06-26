169473941781a2c4aac82083b26989742a936679
yiyun Ding <amydingyiyun@gmail.com>
2020-06-26 13:08:07 -0400

    yiyun

--- phase1/src/TradeManager.java
+++ phase1/src/TradeManager.java
@@ -9,26 +9,61 @@ public class TradeManager {
     ArrayList<Trade> tradeList;
 
 
+    /**
+     * constructor, tradeList is an instance variable that stores all the Trade.
+     */
     public TradeManager(){
         tradeList = new ArrayList<Trade>();
     }
 
-    public Trade createOnewayTrade(String tradeType, String duration){
 
+    /**
+     * if the User wants to make a one way trade, tradeManager will create a one way trade
+     * @param lender
+     * @param borrower
+     * @param item
+     * @param duration
+     * @return
+     */
+    public Trade createOnewayTrade(ClientUser lender, ClientUser borrower, Item item, int duration){
+        OnewayTrade newTrade = new OnewayTrade(lender, borrower, item, duration);
+        tradeList.add(newTrade);
+        return newTrade;
     }
 
-    public Trade createTwowayTrade(){
-
+    /**
+     * if the User wants to make a two way trade, tradeManager will create a two way trade.
+     * @param user1
+     * @param user2
+     * @param item1to2
+     * @param item2to1
+     * @param duration
+     * @return
+     */
+    public Trade createTwowayTrade(ClientUser user1, ClientUser user2, Item item1to2, Item item2to1, int duration){
+        TwowayTrade newTrade = new TwowayTrade(user1, user2, item1to2, item2to1, duration);
+        tradeList.add(newTrade);
+        return newTrade;
     }
 
+
+    public Trade getTrade(int id) {
+        for (Trade trade : tradeList) {
+            if (trade.getId() == id) {
+                return trade;
+            } else {
+                System.out.println("no id existed"); // need to change to raise XXX error.
+            }
+        }
+    }
 
     /**
      * abstract class
      * make trade happen, move items in User's list
      */
-    public void makeTrade(){
+   // public void makeTrade(){
 
-    };
+    //};
 
 
 
--- phase1/src/TwowayTrade.java
+++ phase1/src/TwowayTrade.java
@@ -11,11 +11,10 @@ public class TwowayTrade extends Trade {
      * @param trader2 Trader who participates in the trade
      * @param item1 the item that trader1 give to trader2
      * @param item2 the item that trader2 give to trader1
-     * @param status whether the trade is unconfirmed, confirmed or completed
      * @param duration whether the trade is temporary or permanent
      */
-    public TwowayTrade(ClientUser trader1, ClientUser trader2, Item item1, Item item2, String status, int duration){
-        super(status, duration);
+    public TwowayTrade(ClientUser trader1, ClientUser trader2, Item item1, Item item2, int duration){
+        super(duration);
         this.trader1 = trader1;
         this.trader2 = trader2;
         item1to2 = item1;
