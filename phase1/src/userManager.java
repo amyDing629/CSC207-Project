afb5cf77090e4e9e3485673fe78cf860ac9a6a30
siwei <sw.tang@mail.utoronto.ca>
2020-06-26 21:22:16 -0400

    nuew use case on 2020-06-26 21:22pm

---
+++ phase1/src/userManager.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class userManager {
+    private static ArrayList<User> userlist = new ArrayList<>();
+
+    public void addUser(User u){
+        userlist.add(u);
+    }
+
+    public User getUser(int id){
+        for(User u : userlist){
+            if(u.getId() == id)
+                return u;
+        }
+        return null;
+    }
+
+    public boolean verifyUser(int id, String password){
+        for(User u : userlist){
+            if(u.getId() ==id && u.getPassword().equals(password))
+                return true;
+        }
+        return false;
+    }
+
+}
+
