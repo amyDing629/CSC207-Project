cc3094d1cd4b7a458805c61e76480ad9590c5168
Ensouled <ensouledw@outlook.com>
2020-06-30 21:03:20 -0400

    upadte to register,login and editinfo

--- phase1/src/Login.java
+++ phase1/src/Login.java
@@ -2,12 +2,14 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Login {
-    public static void main(String[] args) throws IOException {
-        int a=0;
-        while (a!=-1) {
+    public void mainMenu() throws IOException {
+        int a=-1;
+        while (a!=0) {
+            //print out the list of current users-------------------------------
             System.out.println("Users:");
             try {
                 BufferedReader reader = new BufferedReader(new FileReader(
@@ -22,70 +24,135 @@ public class Login {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            System.out.println("\nMenu:\n 1.login\n 2.register");
+            //------------------------------------------------------------------
+            System.out.println("\nMenu:\n 1.login\n 2.register\n 0.quit");
             Scanner sc = new Scanner(System.in);
             System.out.print(">");
             a = sc.nextInt();
             sc.nextLine();
             System.out.println("------------------------------");
             if (a==1){
-                register();
+                login();
             }
             else if (a == 2){
-                System.out.println("Please enter your username!");
-                System.out.print(">");
-                String username = sc.nextLine();
-                System.out.println("Please enter your password!");
-                System.out.print(">");
-                String password = sc.nextLine();
-                //usermanger username verification?
-                FileWriter fw = new FileWriter("username.txt");
-                fw.write(username);
-                fw.close();
-                ClientUser user1 = new ClientUser(username, password);
-                System.out.println("Your account has been successfully created!");
-                System.out.println("Your id: " + user1.getId());
-                System.out.println("Your username: " + user1.getUsername());
-                System.out.println("Your password: " + user1.getPassword());
+                register();
             }
             System.out.println("------------------------------");
         }
 
     }
-    public static void register(){
+    public void login() throws IOException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your account username!");
-        System.out.print(">");
-        String username = sc.nextLine();
-        System.out.println("Please enter your password!");
-        System.out.print(">");
-        String password = sc.nextLine();
+        int input=0;
+        while (input==0) {
+            System.out.println("Please enter your account username!");
+            System.out.print(">");
+            String username = sc.nextLine();
+            System.out.println("Please enter your password!");
+            System.out.print(">");
+            String password = sc.nextLine();
 
-        //usermanger account verification? and returns a user object.
-
-        System.out.println("Notifications:\n");// display all user's notifications.
-
-        System.out.println("Actions:\n1.Trade\n2.Message");
+            //usermanger account verification? and returns a user object.
+            if (UserManager.verifyUser(username, password)) {
+                int exit = 0;
+                while (exit != 1) {
+                    System.out.println("Notifications:\n");// display all user's notifications.
+                    System.out.println("Actions:\n1.Edit information\n2.Message\n3.Inventory\n4.Message\n5.Trade History\n0.quit to menu");
+                    System.out.print(">");
+                    int op = sc.nextInt();
+                    sc.nextLine();
+                    if (op == 1) {
+                        editInfo(UserManager.getUser(username));
+                    } else if (op == 2) {
+                        message();
+                    } else if (op == 3) {
+                        inventory();
+                    } else if (op == 4) {
+                        message();
+                    } else if (op == 5) {
+                        tradeHistory();
+                    } else if (op == 0) {
+                        exit = 1;
+                    } else {
+                        System.out.println("Sorry I dont undertand your command, plz enter it again");
+                    }
+                }
+            } else {
+                System.out.println("You have incorrect username or password, please try to login again, enter any number to continue. enter 1 to exit.");
+                input=sc.nextInt();
+                sc.nextLine();
+            }
+        }
     }
-    public static void login() throws IOException {
+    public void register() throws IOException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your username!");
-        System.out.print(">");
-        String username = sc.nextLine();
-        System.out.println("Please enter your password!");
-        System.out.print(">");
-        String password = sc.nextLine();
-        //usermanger username verification?
-        FileWriter fw = new FileWriter("username.txt");
-        fw.write(username);
-        fw.close();
-        ClientUser user1 = new ClientUser(username, password);
-        System.out.println("Your account has been successfully created!");
-        System.out.println("Your id: " + user1.getId());
-        System.out.println("Your username: " + user1.getUsername());
-        System.out.println("Your password: " + user1.getPassword());
+        int input=sc.nextInt();
+        sc.nextLine();
+        while(input!=1) {
+            System.out.println("Please enter your username!");
+            System.out.print(">");
+            String username = sc.nextLine();
+            System.out.println("Please enter your password!");
+            System.out.print(">");
+            String password = sc.nextLine();
+            //usermanger username verification?
+            if (UserManager.getUser(username) == null) {
+                ClientUser user1 = new ClientUser(username, password, false);
+                System.out.println("Your account has been successfully created!");
+                System.out.println("Your id: " + user1.getId());
+                System.out.println("Your username: " + user1.getUsername());
+                System.out.println("Your password: " + user1.getPassword());
+            }
+            else{
+                System.out.println("The username already exists, please try to register again, enter any number to continue. Enter 1 to exit.");
+                input=sc.nextInt();
+                sc.nextLine();
+            }
+        }
     }
-            System.out.println("------------------------------");
+
+    public void editInfo(User user){
+        Scanner sc=new Scanner(System.in);
+        int exit=-1;
+        while(exit!=0) {
+            System.out.println("Hello,user," + user.getUsername());
+            System.out.println("Actions:\n1.Change username\n2.Change password\n");
+            if (user.getIsAdmin() == true) {
+                System.out.print("3.Freeze a user\n4.Change user's limit\n5.Set user into admin\n");
+            }
+            System.out.println("0. exit");
+            int input = sc.nextInt();
+            sc.nextLine();
+            System.out.println("-----------------------------")
+            switch (input) {
+                case 1:
+                    System.out.println("Change username");
+                    break;
+                case 2:
+                    System.out.println("Change password");
+                    break;
+                case 3:
+                    System.out.println("Freeze a user");
+                    break;
+                case 4:
+                    System.out.println("Change user's limit");
+                    break;
+                case 5:
+                    System.out.println("Set user into admin");
+                    break;
+                case 0:
+                    exit=0;
+                    break;
+            }
+        }
+    }
+    public static void inventory(){
+
+    }
+    public static void message(){
+
+    }
+    public static void tradeHistory(){
+
     }
 }
-
