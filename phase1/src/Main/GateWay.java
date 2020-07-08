c9e2da6aa4d3d692609416503a8ebf092284b6ea
siwei <sw.tang@mail.utoronto.ca>
2020-07-08 19:49:19 -0400

    a little modificatopn

--- phase1/src/Main/GateWay.java
+++ phase1/src/Main/GateWay.java
@@ -3,11 +3,12 @@ package Main;
 import Inventory.Item;
 import Trade.Trade;
 import User.User;
+import com.sun.deploy.net.MessageHeader;
 
 import java.util.ArrayList;
 
 public class GateWay {
-    static ArrayList<User> users = new ArrayList<User>();
-    static ArrayList<Trade> trades = new ArrayList<Trade>();
-    static ArrayList<Item> inventory = new ArrayList<>();
+    public static ArrayList<User> users = new ArrayList<User>();
+    public static ArrayList<Trade> trades = new ArrayList<Trade>();
+    public static ArrayList<Item> inventory = new ArrayList<>();
 }
