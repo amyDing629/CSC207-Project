f18d2cb36329722e7eceb696bcc8e43e0cb3349a
yiyun Ding <amydingyiyun@gmail.com>
2020-07-08 19:34:27 -0400

    gateway

---
+++ phase1/src/GateWay.java
@@ -0,0 +1,11 @@
+import Inventory.Item;
+import Trade.Trade;
+import User.User;
+
+import java.util.ArrayList;
+
+public class GateWay {
+    static ArrayList<User> users = new ArrayList<User>();
+    static ArrayList<Trade> trades = new ArrayList<Trade>();
+    static ArrayList<Item> inventory = new ArrayList<Item>();
+}
