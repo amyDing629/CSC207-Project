23243e2bc049486c0ea74bfe2c77ecae51d80e9d
yiyun Ding <amydingyiyun@gmail.com>
2020-07-08 21:11:41 -0400

    tradeDataAccess

--- phase1/src/Trade/TradeManager.java
+++ phase1/src/Trade/TradeManager.java
@@ -2,6 +2,7 @@ package Trade;
 
 import Inventory.Inventory;
 import Inventory.Item;
+import Main.GateWay;
 import Trade.MeetingSystem.MeetingEditor;
 import Trade.MeetingSystem.Meeting;
 import Trade.MeetingSystem.MeetingStatus;
@@ -23,7 +24,7 @@ import java.util.UUID;
  * Automatically update the trade history for both users in the trade.
  */
 public class TradeManager {
-    private ArrayList<Trade> tradeList;
+    //private ArrayList<Trade> tradeList;
     private UserManager userManager;
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
@@ -34,13 +35,11 @@ public class TradeManager {
      * currentUser is the user.User who is currently interacting with this trade.TradeManager.
      */
     public TradeManager() throws IOException {
-        tradeList = new ArrayList<>();
-        readFile();
         userManager = new UserManager();
     }
 
     public ArrayList<Trade> getTradeList(){
-        return tradeList;
+        return GateWay.trades;
     }
 
 
@@ -54,8 +53,7 @@ public class TradeManager {
     public Trade createOnewayTrade(UUID currUserId, UUID otherUserId, Item item, int duration, LocalDateTime time)
             throws IOException {
         OnewayTrade newTrade = new OnewayTrade(currUserId, otherUserId, item, duration, time);
-        tradeList.add(newTrade);
-        updateFile();
+        GateWay.trades.add(newTrade);
         // Record this new trade.Trade in system
 
         //Update trade history for both users
@@ -74,8 +72,7 @@ public class TradeManager {
     public void createTwowayTrade(UUID currUserId, UUID otherUserId, Item item1to2, Item item2to1, int duration,
                                   LocalDateTime time) throws IOException {
         TwowayTrade newTrade = new TwowayTrade(currUserId, otherUserId, item1to2, item2to1, duration, time);
-        tradeList.add(newTrade);
-        updateFile();
+        GateWay.trades.add(newTrade);
         // Update trade history for both users
         updateTradeHistory(currUserId, otherUserId, newTrade);
     }
@@ -83,19 +80,18 @@ public class TradeManager {
 
 
     public void updateTradeHistory(UUID currUserId, UUID tarUserId, Trade newTrade) throws IOException {
-       // System.out.println("userList:"+userManager.getUserList());
+        // System.out.println("userList:"+userManager.getUserList());
         User currentUser = userManager.getUser(currUserId);
         User tarUser = userManager.getUser(tarUserId);
         System.out.println(currentUser);
-        System.out.println(userManager.getUserList());
+        System.out.println(GateWay.trades);
         currentUser.getTradeHistory().add(newTrade.getId());
         tarUser.getTradeHistory().add(newTrade.getId());
-        userManager.updateFile();
     }
 
 
     public Trade getTrade(UUID id) {
-        for (Trade trade : tradeList) {
+        for (Trade trade : GateWay.trades) {
             if (trade.getId().equals(id)) {
                 return trade;
             }
@@ -104,158 +100,10 @@ public class TradeManager {
     }
 
 
-    public void readFile(){
-        Trade trade;
-        try {
-            Inventory iv = new Inventory();
-            BufferedReader reader = new BufferedReader(new FileReader("phase1/src/trade.txt"));
-            String line = reader.readLine();
-            while (line != null) {
-                String[] lst = line.split(",");
-                UUID tradeId = UUID.fromString(lst[0]);
-                UUID user1Id = UUID.fromString(lst[3]);
-                UUID user2Id = UUID.fromString(lst[4]);
-                int duration = Integer.parseInt(lst[2]);
-                ArrayList<UUID> users = new ArrayList<UUID>();
-                users.add(user1Id);
-                users.add(user2Id);
-                LocalDateTime tradeTime = LocalDateTime.parse(lst[10],formatter);
-                Item item1 = iv.getItem(lst[5]);
-                String fstMeeting = lst[7];
-                String scdMeeting = lst[8];
-                HashMap<UUID, Boolean> idToC = new HashMap<>();
-                HashMap<UUID, MeetingEditor> idToE = new HashMap<>();
-                HashMap<UUID, Boolean> idToA = new HashMap<>();
-
-                if (lst[1].equals("oneway")){
-                    trade = new OnewayTrade(user1Id,user2Id,item1,duration,tradeTime);
 
 
-                }else{
-                    Item item2 = iv.getItem(lst[6]);
-                    trade = new TwowayTrade(user1Id,user2Id,item1,item2,duration,tradeTime);
-                }
-                if (!fstMeeting.equals("null")) {
-                    String[] fm = fstMeeting.split("/");
-                    LocalDateTime fmTime = LocalDateTime.parse(fm[0], formatter);
-                    trade.setMeeting(fmTime, fm[1], users);
-                    //set confirmed status
-                    String[] confirmMap = fm[4].split(";");
-                    idToC.put(user1Id,Boolean.parseBoolean(confirmMap[0]));
-                    idToC.put(user2Id,Boolean.parseBoolean(confirmMap[1]));
-                    trade.getMeeting().setConfirmedStatusFull(idToC);
-                    //set agreed status
-                    String[] agreeMap = fm[5].split(";");
-                    idToA.put(user1Id,Boolean.parseBoolean(agreeMap[0]));
-                    idToA.put(user2Id,Boolean.parseBoolean(agreeMap[1]));
-                    trade.getMeeting().setAgreedStatusFull(idToA);
-                    //set edition time
-                    String[] editTime = fm[3].split(";");
-                    MeetingEditor me1 = new MeetingEditor(user1Id);
-                    me1.setTimeOfEdition(Integer.parseInt(editTime[0]));
-                    MeetingEditor me2 = new MeetingEditor(user2Id);
-                    me2.setTimeOfEdition(Integer.parseInt(editTime[1]));
-                    idToE.put(user1Id,me1);
-                    idToE.put(user2Id,me2);
-                    trade.getMeeting().setIdToEditor(idToE);
-                    trade.getMeeting().setStatus(MeetingStatus.valueOf(fm[2]));
-
-                }
-                if (!scdMeeting.equals("null")){
-                    String[] sm = scdMeeting.split("/");
-                    LocalDateTime smTime = LocalDateTime.parse(sm[0],formatter);
-                    trade.setSecondMeeting(smTime, sm[1], users);
-                    trade.getSecondMeeting().setStatus(MeetingStatus.valueOf(sm[2]));
-                    String[] confirmMap = sm[3].split(";");
-                    idToC.put(user1Id,Boolean.parseBoolean(confirmMap[0]));
-                    idToC.put(user2Id,Boolean.parseBoolean(confirmMap[1]));
-                    trade.getSecondMeeting().setConfirmedStatusFull(idToC);
-                }
-
-                trade.setStatus(lst[9]);
-                trade.setId(tradeId);
-                tradeList.add(trade);
-                line = reader.readLine();
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public void addTradeToFile(Trade trade) throws IOException{
-        try {
-            FileOutputStream fos = new FileOutputStream("phase1/src/trade.txt", true);
-            UUID id = trade.getId();
-            String type;
-            type = trade.getType();
-            Integer duration = trade.getDuration();
-            UUID user1 = trade.getUsers().get(0);
-            UUID user2 = trade.getUsers().get(1);
-            ArrayList<Item> items = trade.getItemList();
-            String item1; String item2;
-            if (items.size() == 1){
-                item1 = items.get(0).getName();
-                item2 = null;
-            }else{
-                item1 = items.get(0).getName();
-                item2 = items.get(1).getName();
-            }
-            Meeting fm = trade.getMeeting();
-            String fmStr = null;
-            if (fm != null){
-                HashMap<UUID, MeetingEditor> idToE = fm.getIdToEditor();
-                String idToEdStr = idToE.get(user1).getTimeOfEdition() + ";"+
-                        idToE.get(user2).getTimeOfEdition();
-                HashMap<UUID, Boolean> conStatus = fm.getConfirmedStatusFull();
-                String idToCoStr = conStatus.get(user1) + ";" + conStatus.get(user2);
-                HashMap<UUID, Boolean> agreeStatus = fm.getAgreedStatusFull();
-                String idToAgreeStr = agreeStatus.get(user1) + ";" + agreeStatus.get(user2);
-                //2020-06-30 11:49/home/incomplete/0;0/false;false
-                fmStr = fm.getDateTime().format(formatter)+"/"+fm.getPlace()+"/"+fm.getStatus()
-                        +"/"+idToEdStr+"/"+ idToCoStr+"/"+idToAgreeStr;
-
-            }
-            Meeting sm = trade.getSecondMeeting();
-            String smStr;
-            if (sm == null){
-                smStr = null;
-            }else{
-                HashMap<UUID, Boolean> conStatus = sm.getConfirmedStatusFull();
-                String idToCoStr = conStatus.get(user1) + ";" + conStatus.get(user2);
-                smStr = sm.getDateTime().format(formatter)+"/"+sm.getPlace()+"/"+sm.getStatus()+"/"+idToCoStr;
-            }
-            String status = trade.getStatus();
-            String time = trade.getCreateTime().format(formatter);
-
-            fos.write((id+","+type+","+duration+","+user1+","+user2+","+item1+","+item2+","+fmStr+","
-                    +smStr+","+status+","+time+"\n").getBytes());
-            fos.close();
-        }catch(IOException e){
-            System.out.println("cannot edit file");
-        }
-    }
 
 
-    public void updateFile() throws IOException {
-        File file = new File("phase1/src/trade.txt");
-        try {
-            if(!file.exists()) {
-                boolean result = file.createNewFile();
-            }
-            FileWriter fileWriter =new FileWriter(file);
-            fileWriter.write("");
-            fileWriter.flush();
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (tradeList.size()>0){
-            for (Trade trade: tradeList){
-                addTradeToFile(trade);
-            }
-        }
-    }
 
 }
+
--- phase1/src/Trade/tradeMain.java
+++ phase1/src/Trade/tradeMain.java
@@ -1,10 +1,12 @@
 package Trade;
 
-import Inventory.Inventory;
+import Inventory.*;
+import Main.GateWay;
 import User.User;
 import User.UserManager;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -27,7 +29,7 @@ public class tradeMain {
         amy.getWishBorrow().add("peach");
         trade.RequestTradeUI rtp = new trade.RequestTradeUI(amy, peach);
         rtp.run();
-        */
+
         ArrayList<User> lst = um.splitUser(um.readFile());
         User amy = lst.get(0);
         User daniel = lst.get(1);
@@ -62,6 +64,33 @@ public class tradeMain {
 
          */
 
+        User amy = new User("amy","1234", false);
+        User daniel = new User("daniel","345", false);
+        GateWay.users.add(amy);
+        GateWay.users.add(daniel);
+        Trade trade = new TradeManager().getTradeList().get(0);
+        new TradeManager().updateTradeHistory(amy.getId(), daniel.getId(), trade);
+        /*
+        um.getUserList().add(amy);
+        um.getUserList().add(daniel);
+        //ArrayList<User> lst = um.splitUser(um.readFile());
+        //User amy = lst.get(0);
+        Item peach = new Inventory().getLendingList().get(0);
+        //User daniel = lst.get(1);
+        //Trade trade = tm.createOnewayTrade(amy.getId(), daniel.getId(), peach, -1, LocalDateTime.now());
+        //tm.addTradeToFile(trade);
+        System.out.println("amy trade history: " + amy.getTradeHistory());
+        System.out.println("daniel trade history: " + daniel.getTradeHistory());
+        tm.createOnewayTrade(amy.getId(), daniel.getId(), peach, -1, LocalDateTime.now());
+        System.out.println("amy trade history: " + amy.getTradeHistory());
+        System.out.println("daniel trade history: " + daniel.getTradeHistory());
+
+
+        Trade trade = new TradeManager().getTradeList().get(0);
+        amy.getTradeHistory().add(trade.getId());
+        System.out.println(amy.getTradeHistory());
+
+         */
 
 
 
