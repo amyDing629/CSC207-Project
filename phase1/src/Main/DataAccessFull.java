7cab7a99662044397fbd37a63a5bc8c64fe3b91f
yiyun Ding <amydingyiyun@gmail.com>
2020-07-08 21:30:56 -0400

    data access full

---
+++ phase1/src/Main/DataAccessFull.java
@@ -0,0 +1,27 @@
+package Main;
+
+import Inventory.InvDataAccess;
+import Trade.TradeDataAccess;
+import User.FileEditor;
+
+import java.io.IOException;
+
+public class DataAccessFull {
+    private final InvDataAccess ida = new InvDataAccess();
+    private final TradeDataAccess tda = new TradeDataAccess();
+    private final FileEditor fe = new FileEditor();
+
+    public void updateFile() throws IOException {
+        ida.updateFile();
+        tda.updateFile();
+        fe.updateFile();
+    }
+
+    public void readFile() throws IOException {
+        ida.readFile();
+        tda.readFile();
+        fe.readFile();
+    }
+
+
+}
