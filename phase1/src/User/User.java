fd6b1c30180bf4c6854025f9a1726f0bf070941a
siwei <sw.tang@mail.utoronto.ca>
2020-07-12 20:29:49 -0400

    add javadoc

--- phase1/src/User/User.java
+++ phase1/src/User/User.java
@@ -86,20 +86,31 @@ public class User {
         id = UUID.randomUUID();
     }
 
-
+    /**
+     * return the password of the user
+     */
     public String getPassword() {
         return password;
     }
+    /**
+     * return the name of the user
+     */
     public String getUsername(){return username;}
-
+    /**
+     * return the id of the user
+     */
     public UUID getId() {
         return id;
     }
-
+    /**
+     * return whether the user account is being frozen
+     */
     public boolean getIsFrozen(){
         return isFrozen;
     }
-
+    /**
+     * return all the notifications the user has
+     */
     public List<String> getNotification(){return notification;}
 
     /**
@@ -115,20 +126,26 @@ public class User {
      * set the input id for the user
      */
     public void setId(UUID a){this.id = a;}
-
+    /**
+     * @param weekTransaction  the week transaction limit we want to set for the user
+     * set the week transaction limit for the user
+     */
     public void setWeekTransactionLimit(int weekTransaction){
         this.weekTransactionLimit = weekTransaction;
     }
 
     /**
      * @param hi the name of the item
-     * the name of item that the user wants to lend
+     * add the name of item that the user wants to lend into list
      */
     public void addWishes(String hi){
         this.wishLend.add(hi);
     }
 
-
+    /**
+     * @param name the name of the item
+     * add the name of item that the user wants to borrow into list
+     */
     public void addWishBorrow(String name){
         this.wishBorrow.add(name);
     }
@@ -148,40 +165,82 @@ public class User {
         this.wishLend.remove(hi);
     }
 
+    /**
+     * @param wishLend the list of items the user wants to lend
+     * set the list of items that the user wants to lend as wishlend list
+     */
     public void setWishLend(List<String> wishLend){
         this.wishLend = wishLend;
     }
 
+    /**
+     * @param notification all the notifications the user has
+     * set all the notifications as the list
+     */
     public void setNotification(List<String> notification){
         this.notification = notification;
     }
 
+    /**
+     * @param diff the number of the difference between number of borrowed items and lend items
+     * set the integer as the number of the difference between number of borrowed items and lend items
+     */
     public void setDiff(int diff) {
         this.diff = diff;
     }
 
+    /**
+     * @param tradeHistory all the trades' ids that the user has
+     * set the user's trade history as the list of ids
+     */
     public void setTradeHistory(List<UUID> tradeHistory) {
         this.tradeHistory = tradeHistory;
     }
 
+    /**
+     * get all the trades' ids the user has
+     */
     public List<UUID> getTradeHistory(){
         return tradeHistory;
     }
 
+    /**
+     * @param id the trade id the user wants to add
+     * add the trade id into user's trade history
+     */
     public void addTradeHistory(UUID id){tradeHistory.add(id);}
 
+    /**
+     * return whether the user is administrative user or not
+     */
     public boolean getIsAdmin(){return isAdmin;}
 
+    /**
+     * return the list of items that the user wants to lend
+     */
     public List<String> getWishLend() {
         return wishLend;
     }
+
+    /**
+     * return the list of items that the user wants to borrow
+     */
     public List<String> getWishBorrow() {
         return wishBorrow;
     }
+
+    /**
+     * @param aTrue the status of the account the user want to set
+     * set the status of the account of user to be aTrue
+     */
     public void setFrozen(boolean aTrue){
         isFrozen = aTrue;
     }
 
+    /**
+     * @param borrow the status of the account the user want to set
+     * set the status of the account of user to be borrow
+     */
     public void setBorrow(boolean borrow){
 
         isBorrow = borrow;
