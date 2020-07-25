65bcdabf0c65ad12d6c83e2c9d9db5b56c5dccf8
yangyu77 <amber.yang@mail.utoronto.ca>
2020-07-25 01:33:11 -0400

    Start PointManager

---
+++ phase2/src/User/PointSystem/PointManager.java
@@ -0,0 +1,40 @@
+package User.PointSystem;
+
+import User.ClientUser;
+import User.UserManager;
+import java.util.UUID;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * [Use Case] class
+ * Responsible for managing award points of all users
+ */
+public class PointManager {
+    private UserManager um;
+    private HashMap<UUID, Integer> pointList;
+
+    public PointManager(){
+        this.pointList = new HashMap<>();
+    }
+
+    /***/
+    public HashMap<UUID, Integer> getPointList() {return this.pointList;}
+
+    /**
+     * Set up the point list or update the list when new users are recorded in UserManager.
+     */
+    public void setPointList() {
+        for (ClientUser user: this.um.getUserList()) {
+            if (!this.pointList.containsKey(user.getId())) {
+                this.pointList.put(user.getId(), 0);
+            }
+        }
+    }
+
+    //TODO: Complete
+    public HashMap<UUID, Integer> sortListByPoints() {}
+
+    //TODO: Complete
+    public UUID getMostFreq() {}
+}
