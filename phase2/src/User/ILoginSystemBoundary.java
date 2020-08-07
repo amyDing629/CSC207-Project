b09ececd286bcfa376f09e7424cb2872677df23d
ChangyanXu <changyan.xu@mail.utoronto.ca>
2020-08-07 01:31:47 -0400

    Visitor - LoginIGUI (xcy)

--- phase2/src/User/ILoginSystemBoundary.java
+++ phase2/src/User/ILoginSystemBoundary.java
@@ -1,6 +1,11 @@
 package User;
 
 public interface ILoginSystemBoundary {
+
     void register(String name, String password);
+
     boolean login(String name, String password);
+
+    void explore();
+
 }
--- phase2/src/User/mainRun.java
+++ phase2/src/User/mainRun.java
@@ -1,25 +1,26 @@
 package User;
 
-import Inventory.*;
-//import Main.DataAccessFull;
+import Inventory.Inventory;
 import Main.UI.UIcontoller;
 import Trade.TradeManager;
 
 import java.io.File;
 
+//import Main.DataAccessFull;
+
 public class mainRun {
 
-    public static void main(String args[]){
-        Inventory iv=new Inventory();
+    public static void main(String[] args) {
+        Inventory iv = new Inventory();
         UserManager um = new UserManager();
         TradeManager tm = new TradeManager();
         ItemApprovalManager iam = new ItemApprovalManager();
-        //DataAccessFull uaf = new DataAccessFull(um, tm, iv, iam);
-        AdminActivityManager aam=new AdminActivityManager(tm,um);
-        UIcontoller uc=new UIcontoller(um,aam,tm,iam,iv);
-        uc.checkFileEmpty(new File("phase1/src/username.txt"));
+//        DataAccessFull uaf = new DataAccessFull(um, tm, iv, iam);
+        AdminActivityManager aam = new AdminActivityManager(tm, um);
+        UIcontoller uc = new UIcontoller(um, aam, tm, iam, iv);
+        uc.checkFileEmpty(new File("phase2/src/username.txt"));
         //uaf.readFile(tm,iv,um);
-        LoginIGUI lo=new LoginIGUI(um,tm,iv,iam,uc,aam);
+        LoginIGUI lo = new LoginIGUI(um, tm, iv, iam, uc, aam);
         lo.run();
     }
 }
