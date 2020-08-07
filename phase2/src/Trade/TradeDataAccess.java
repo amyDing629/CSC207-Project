9d0842ad8e8c59cfe8c18a4152441ca1cc094066
yiyun Ding <amydingyiyun@gmail.com>
2020-08-07 01:22:16 -0400

    dataAccess

--- phase2/src/Trade/TradeDataAccess.java
+++ phase2/src/Trade/TradeDataAccess.java
@@ -14,100 +14,98 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
-///**
-// * read and update trade.txt file (part of job from tradeManager)
-// */
-//public class TradeDataAccess {
-//    DateTime dt = new DateTime();
-//    DateTimeFormatter formatter = dt.getFormat(); // yyyy-MM-dd HH:mm
-//    TradeManager tm;
-//
-//    public TradeDataAccess(TradeManager tm){
-//        this.tm = tm;
-//    }
-//
-//    /**
-//     * read info from trade.txt to trade list in gateway
-//     */
-//    /*
-//    public void readFile(Inventory iv) {
-//        Trade trade;
-//        try {
-//            BufferedReader reader = new BufferedReader(new FileReader("phase2/src/trade.txt"));
-//            String line = reader.readLine();
-//            while (line != null) {
-//                String[] lst = line.split(",");
-//                UUID tradeId = UUID.fromString(lst[0]);
-//                UUID user1Id = UUID.fromString(lst[3]);
-//                UUID user2Id = UUID.fromString(lst[4]);
-//                int duration = Integer.parseInt(lst[2]);
-//                ArrayList<UUID> users = new ArrayList<>();
-//                users.add(user1Id);
-//                users.add(user2Id);
-//                LocalDateTime tradeTime = LocalDateTime.parse(lst[10],formatter);
-//                Item item1 = iv.getItem(lst[5]);
-//                String fstMeeting = lst[7];
-//                String scdMeeting = lst[8];
-//                HashMap<UUID, Boolean> idToC = new HashMap<>();
-//                HashMap<UUID, MeetingEditor> idToE = new HashMap<>();
-//                HashMap<UUID, Boolean> idToA = new HashMap<>();
-//
-//                if (lst[1].equals("oneway")){
-//                    trade = new OnewayTrade(user1Id,user2Id,item1,duration,tradeTime);
-//
-//
-//                }else{
-//                    Item item2 = iv.getItem(lst[6]);
-//                    trade = new TwowayTrade(user1Id,user2Id,item1,item2,duration,tradeTime);
-//                }
-//                if (!fstMeeting.equals("null")) {
-//                    String[] fm = fstMeeting.split("/");
-//                    LocalDateTime fmTime = LocalDateTime.parse(fm[0], formatter);
-//                    trade.setMeeting(fmTime, fm[1], users);
-//                    //set confirmed status
-//                    String[] confirmMap = fm[4].split(";");
-//                    idToC.put(user1Id,Boolean.parseBoolean(confirmMap[0]));
-//                    idToC.put(user2Id,Boolean.parseBoolean(confirmMap[1]));
-//                    trade.getMeeting().setConfirmedStatusFull(idToC);
-//                    //set edition time
-//                    String[] editTime = fm[3].split(";");
-//                    MeetingEditor me1 = new MeetingEditor(user1Id);
-//                    me1.setTimeOfEdition(Integer.parseInt(editTime[0]));
-//                    MeetingEditor me2 = new MeetingEditor(user2Id);
-//                    me2.setTimeOfEdition(Integer.parseInt(editTime[1]));
-//                    idToE.put(user1Id,new MeetingEditor(user1Id));
-//                    idToE.put(user2Id,new MeetingEditor(user2Id));
-//                    //set agree status
-//                    String[] agreeMap = fm[5].split(";");
-//                    idToA.put(user1Id,Boolean.parseBoolean(agreeMap[0]));
-//                    idToA.put(user2Id,Boolean.parseBoolean(agreeMap[1]));
-//                    trade.getMeeting().setAgreedStatusFull(idToA);
-//                    trade.getMeeting().setIdToEditor(idToE);
-//                    trade.getMeeting().setStatus(MeetingStatus.valueOf(fm[2]));
-//                    trade.getMeeting().changeLastEditUser(UUID.fromString(fm[6]));
-//                }
-//                if (!scdMeeting.equals("null")){
-//                    String[] sm = scdMeeting.split("/");
-//                    LocalDateTime smTime = LocalDateTime.parse(sm[0],formatter);
-//                    trade.setSecondMeeting(smTime, sm[1], users);
-//                    trade.getSecondMeeting().setStatus(MeetingStatus.valueOf(sm[2]));
-//                }
-//
-//                trade.setStatus(TradeStatus.valueOf(lst[9]));
-//                trade.setId(tradeId);
-//                trade.setCreator(UUID.fromString(lst[11]));
-//                tm.getTradeList().add(trade);
-//                line = reader.readLine();
-//            }
-//            reader.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
-//     */
-//
+
+public class TradeDataAccess {
+    DateTime dt = new DateTime();
+    DateTimeFormatter formatter = dt.getFormat(); // yyyy-MM-dd HH:mm
+    TradeManager tm;
+
+    public TradeDataAccess(TradeManager tm){
+        this.tm = tm;
+    }
+
+    /**
+     * read info from trade.txt to trade list in gateway
+     */
+    /*
+    public void readFile(Inventory iv) {
+        Trade trade;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("phase2/src/trade.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                String[] lst = line.split(",");
+                UUID tradeId = UUID.fromString(lst[0]);
+                UUID user1Id = UUID.fromString(lst[3]);
+                UUID user2Id = UUID.fromString(lst[4]);
+                int duration = Integer.parseInt(lst[2]);
+                ArrayList<UUID> users = new ArrayList<>();
+                users.add(user1Id);
+                users.add(user2Id);
+                LocalDateTime tradeTime = LocalDateTime.parse(lst[10],formatter);
+                Item item1 = iv.getItem(lst[5]);
+                String fstMeeting = lst[7];
+                String scdMeeting = lst[8];
+                HashMap<UUID, Boolean> idToC = new HashMap<>();
+                HashMap<UUID, MeetingEditor> idToE = new HashMap<>();
+                HashMap<UUID, Boolean> idToA = new HashMap<>();
+
+                if (lst[1].equals("oneway")){
+                    trade = new OnewayTrade(user1Id,user2Id,item1,duration,tradeTime);
+
+
+                }else{
+                    Item item2 = iv.getItem(lst[6]);
+                    trade = new TwowayTrade(user1Id,user2Id,item1,item2,duration,tradeTime);
+                }
+                if (!fstMeeting.equals("null")) {
+                    String[] fm = fstMeeting.split("/");
+                    LocalDateTime fmTime = LocalDateTime.parse(fm[0], formatter);
+                    trade.setMeeting(fmTime, fm[1], users);
+                    //set confirmed status
+                    String[] confirmMap = fm[4].split(";");
+                    idToC.put(user1Id,Boolean.parseBoolean(confirmMap[0]));
+                    idToC.put(user2Id,Boolean.parseBoolean(confirmMap[1]));
+                    trade.getMeeting().setConfirmedStatusFull(idToC);
+                    //set edition time
+                    String[] editTime = fm[3].split(";");
+                    MeetingEditor me1 = new MeetingEditor(user1Id);
+                    me1.setTimeOfEdition(Integer.parseInt(editTime[0]));
+                    MeetingEditor me2 = new MeetingEditor(user2Id);
+                    me2.setTimeOfEdition(Integer.parseInt(editTime[1]));
+                    idToE.put(user1Id,new MeetingEditor(user1Id));
+                    idToE.put(user2Id,new MeetingEditor(user2Id));
+                    //set agree status
+                    String[] agreeMap = fm[5].split(";");
+                    idToA.put(user1Id,Boolean.parseBoolean(agreeMap[0]));
+                    idToA.put(user2Id,Boolean.parseBoolean(agreeMap[1]));
+                    trade.getMeeting().setAgreedStatusFull(idToA);
+                    trade.getMeeting().setIdToEditor(idToE);
+                    trade.getMeeting().setStatus(MeetingStatus.valueOf(fm[2]));
+                    trade.getMeeting().changeLastEditUser(UUID.fromString(fm[6]));
+                }
+                if (!scdMeeting.equals("null")){
+                    String[] sm = scdMeeting.split("/");
+                    LocalDateTime smTime = LocalDateTime.parse(sm[0],formatter);
+                    trade.setSecondMeeting(smTime, sm[1], users);
+                    trade.getSecondMeeting().setStatus(MeetingStatus.valueOf(sm[2]));
+                }
+
+                trade.setStatus(TradeStatus.valueOf(lst[9]));
+                trade.setId(tradeId);
+                trade.setCreator(UUID.fromString(lst[11]));
+                tm.getTradeList().add(trade);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+     */
+
 //    /**
 //     * add trade in trade list of gateway to trade.txt
 //     * @param trade the trade wants to be added
@@ -166,58 +164,58 @@ import java.util.UUID;
 //            System.out.println("cannot edit file");
 //        }
 //    }
-//
-//    /**
-//     * update trade.txt with information in trade list of gateway.
-//     */
-//    public void updateFile(){
-//        File file = new File("phase2/src/trade.txt");
-//        try {
-//            if(!file.exists()) {
-//                boolean result = file.createNewFile();
-//                if (!result){
-//                    System.out.println("the trade file is not updated successfully");
-//                }
-//            }
-//            FileWriter fileWriter =new FileWriter(file);
-//            fileWriter.write("");
-//            fileWriter.flush();
-//            fileWriter.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        serialize();
-//    }
-//
-//    public void serialize(){
-//        try {
-//            FileOutputStream fileOut =
-//                    new FileOutputStream("phase2/src/trade.ser");
-//            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-//            out.writeObject(tm.getTradeList());
-//            out.close();
-//            fileOut.close();
-//        } catch (IOException i) {
-//            i.printStackTrace();
-//        }
-//    }
-//
-//    public ArrayList<Trade> deSerialize(){
-//        try {
-//
-//            FileInputStream fileIn = new FileInputStream("phase2/src/trade.ser");
-//            ObjectInputStream in = new ObjectInputStream(fileIn);
-//            ArrayList<Trade> res = (ArrayList<Trade>)in.readObject();
-//            in.close();
-//            fileIn.close();
-//            return res;
-//        } catch (IOException | ClassNotFoundException i) {
-//            ArrayList<Trade> res = new ArrayList<>();
-//            i.printStackTrace();
-//            return res;
-//        }
-//
-//    }
-//
-//
-//}
+
+    /**
+     * update trade.txt with information in trade list of gateway.
+     */
+    public void updateFile(){
+        File file = new File("phase2/src/trade.txt");
+        try {
+            if(!file.exists()) {
+                boolean result = file.createNewFile();
+                if (!result){
+                    System.out.println("the trade file is not updated successfully");
+                }
+            }
+            FileWriter fileWriter =new FileWriter(file);
+            fileWriter.write("");
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        serialize();
+    }
+
+    public void serialize(){
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream("phase2/src/trade.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(tm.getTradeList());
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public ArrayList<Trade> deSerialize(){
+        try {
+
+            FileInputStream fileIn = new FileInputStream("phase2/src/trade.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            ArrayList<Trade> res = (ArrayList<Trade>)in.readObject();
+            in.close();
+            fileIn.close();
+            return res;
+        } catch (IOException | ClassNotFoundException i) {
+            ArrayList<Trade> res = new ArrayList<>();
+            i.printStackTrace();
+            return res;
+        }
+
+    }
+
+
+}
