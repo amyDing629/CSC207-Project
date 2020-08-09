6758421d72543602a420af5b66d83410ec0e6678
siwei <sw.tang@mail.utoronto.ca>
2020-08-09 00:52:51 -0400

    no error

--- phase2/src/User/Adapter/AdminController.java
+++ phase2/src/User/Adapter/AdminController.java
@@ -22,23 +22,24 @@ public class AdminController extends ClientUserController {
     }
 
 
-    public void checkFrozen(ClientUser user){
+    public void checkFrozen(String username){
+        ClientUser user = (ClientUser) am.userAccess.getObject(username);
         if(um.readDiff(user)>=um.getDiff(user)){
             System.out.println("You have been freeze due to exceed difference between borrow and lend");
-            am.setFreeze(user,true);
+            am.setFreeze(username,true);
         }
         else if(am.incompleteTransaction(user)){
             System.out.println("You have been freeze due to maximum incomplete transaction");
-            am.setFreeze(user,true);
+            am.setFreeze(username,true);
         }
-        else if(am.tradeLimit(user)){
+        else if(am.tradeLimit(username)){
             System.out.println("You have been freeze due to maximum trade limit");
-            am.setFreeze(user,true);
+            am.setFreeze(username,true);
         }
     }
 
     public void createAdmin(ClientUser user, String name, String password) {
-        am.addNewAdmin(user, name, password);
+        am.addNewAdmin(name, password);
     }
 
 }
